154f32415a578dc42be6f16516b7ca994b9c2f87
wsepesi <sepesi.w@wustl.edu>
2024-01-07 12:07:12 -0600

    language change

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <div className="w-full h-[100vh] flex flex-col p-1 items-center justify-center">
-      <p className='text-sm font-mono'>[Yam]🍠 is a bespoke mailroom management software and data platform.</p>
+      <p className='text-sm font-mono'>[Yam]🍠 is a bespoke mailroom management and data platform.</p>
       <p className='text-sm font-mono'>Contact <strong>sales@yam.com</strong> for more information.</p>
     </div>
   )
